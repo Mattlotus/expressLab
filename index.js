5b6f7425b11a8b0037218a51c91f1c75ec456b73
Mattlotus <mattlotus91@gmail.com>
2023-08-08 21:37:06 -0400

    tip percentage

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const app = express()
 app.set("view engine", "jsx")
 app.engine("jsx",require("express-react-views").createEngine())
 
+const PORT = 9000
 
 
-
-// ROUTES -----------------------------------------------
+// ..............................ROUTES
 app.get('/greeting', ( req, res ) => {
     res.send(`<h1>Hello, stranger</h1>`)
 })
 
 app.get(`/greeting/:name`, ( req, res ) => {
-    res.send(`<h1> What's up ${req.params.name}! Its so great to see you </h1>`)
+    res.send(`<h1> Hi  ${req.params.name}! Nice to meet you </h1>`)
 })
 
-
+app.get(`/tip/:total/:tipPercentage`, (req,res)=>{
+    res.send(`<h1> Expected Tip is: ${(req.params.total * (req.params.tipPercentage / 100))}</h1>`)
+})
 
 
 
@@ -46,18 +48,7 @@ app.listen(PORT, ( req, res ) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-const PORT = 4000
+// ...................stays at bottom
 app.listen(PORT,(req,res)=>{
     console.log(`Listening on port ${PORT}`)
 })
